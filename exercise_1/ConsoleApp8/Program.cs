bdfd76c24e2a25209251db58c872a72e60e0a375
collahua <lianacf6@gmail.com>
2025-03-22 20:11:10 -0500

    Add files via upload

---
+++ exercise_1/ConsoleApp8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise1
+{
+    class Program
+    {
+        static void Greet(string name) => Console.WriteLine($"Hello {name}");
+        static void SayGoodbye(string name) => Console.WriteLine($"Goodbye {name}");
+
+        static void Main(string[] args)
+        {
+            /*  Create an Action<string> called messages that:
+                First, stores a method that says "Hello, <name>".
+                Then, stores a method that says "Welcome, <name>".
+                Executes both methods together with the delegate.
+            */
+
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Action<string> message = Greet;
+            message += SayGoodbye;
+
+            message(name);
+
+            Console.ReadKey();
+        }
+    }
+}
